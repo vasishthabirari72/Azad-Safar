db3dc0f8964595e65d16254692b3393063ba4f33
vasishthabirari72 <birarivasishtha@gmail.com>
2025-12-27 11:54:27 +0530

    Fix image rendering and routing for GitHub Pages

--- src/box.jsx
+++ src/box.jsx
@@ -3,10 +3,12 @@ function Card({ place }) {
   return (
     <Link className="card-link" to={`./place/${place.id}`}>
     <div className="card has-overlay">
-      <img
-        src={place.image}
-        alt={place.title}
-        loading="lazy"
+      
+        <img
+  src={`${import.meta.env.BASE_URL}${place.image}`}
+  alt={place.title}
+
+
       />
       <h3 className="card-title">{place.title}</h3>
     </div>
