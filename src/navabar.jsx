a116a303e0ba66e47d2211ceb16c7e06ca77b5ca
vasishthabirari72 <birarivasishtha@gmail.com>
2026-02-19 11:00:48 +0530

    feat:removed the bug in navabar

--- src/navabar.jsx
+++ src/navabar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function Navbar() {
+  const navClassName = ({ isActive }) => (isActive ? "active" : undefined);
+
   return (
     <header className="navbar">
       <div className="navbar-container">
@@ -11,11 +13,12 @@ function Navbar() {
         </div>
 
         <nav className="navbar-links" aria-label="Main navigation">
-          <NavLink to="/">Home</NavLink>
-          <NavLink className="nav-link">Explore</NavLink>
-          <NavLink className="nav-link">Travel Partner</NavLink>
-          <NavLink className="nav-link">Tourist Guides</NavLink>
-          <NavLink  className="nav-link">Contact</NavLink>
+          <NavLink to="/" end className={navClassName}>Home</NavLink>
+          <NavLink to="/explore" className={navClassName}>Explore</NavLink>
+          <NavLink to="/travel-planner" className={navClassName}>Travel Planner</NavLink>
+          <a href="#">Travel Partner</a>
+          <a href="#">Tourist Guides</a>
+          <a href="#">Contact</a>
         </nav>
 
         <div className="navbar-action">
